90f2ab8bbd3852dbae531b5c0d3d7c6d06afa6ce
vikas <vkm9559@gmail.com>
2024-02-06 21:29:19 +0530

    changes

---
+++ src/App.js
@@ -0,0 +1,22 @@
+import "./App.css";
+import About from "./Components/About";
+import Contact from "./Components/Contact";
+import Footer from "./Components/Footer";
+import Home from "./Components/Home";
+import Testimonial from "./Components/Testimonial";
+import Work from "./Components/Work";
+
+function App() {
+  return (
+    <div className="App">
+     <Home/>
+     <About/>
+     <Work/>
+     <Testimonial/>
+     <Contact/>
+     <Footer/>
+    </div>
+  );
+}
+
+export default App;
